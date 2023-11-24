04117c0854cea078e8f3b42c56a79f7fea6a4264
Alfonso, Duplice <alfonso.duplice@accenture.com>
2023-11-24 11:07:56 +0100

    cart page

--- FE/eCommerce/src/app/app-routing.module.ts
+++ FE/eCommerce/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { DetailsProjComponent } from './pages/details-proj/details-proj.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 const routes: Routes = [
   {path: '', component: HomePageComponent },
   {path: 'details-proj', component: DetailsProjComponent },
+  {path: 'carrello', component: CartComponent },
 ];
 
 @NgModule({
